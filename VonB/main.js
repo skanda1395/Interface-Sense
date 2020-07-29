a3ee328265158ebcd63f28ae6e2e181f8d359cda
Sunil Skanda <skanda1395@gmail.com>
2020-07-29 20:33:39 +0530

    added functionality to display risk status accordingly from API

--- VonB/main.js
+++ VonB/main.js
@@ -1,7 +1,10 @@
 const app = new Vue({
   el: "#app",
   data: {
-    navWidth: "0%"
+    navWidth: "0%",
+    riskStatus: "",
+    networkError: true,
+    codeExpired: true
   },
   methods: {
     openNav() {
@@ -10,6 +13,30 @@ const app = new Vue({
     closeNav() {
       this.navWidth = "0%";
     }
+  },
+  created() {
+    // Get the hash from the URL
+    let url_string = window.location.href;
+    let hash = new URL(url_string).hash;
+
+    // Fetch data associated with the retrieved hash to display risk status
+    fetch("https://us-central1-vonb-app.cloudfunctions.net/userDataforQrcode?qrcode=" + hash)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.text();
+      })
+      .then(data => {
+        this.riskStatus = data.split(' ')[0];
+        console.log(this.riskStatus);
+      })
+      .catch(error => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });    
   },
   mounted() {
     setTimeout(() => $("#myModal").modal("show"), 5000);
