20e8926c523002e9f0ad78b4a06f9e8184b73fab
Sunil Skanda <skanda1395@gmail.com>
2020-07-30 20:40:16 +0530

    draft version of marking attendance functionality

--- Default-PWA/eventDetails.js
+++ Default-PWA/eventDetails.js
@@ -1,7 +1,43 @@
 const app = new Vue({
   el: "#appCapsule",
   data: {
-    event: null
+    event: null,
+    attendanceMarked: null,
+    attendanceMsg: ""
+  },
+  methods: {
+    markAttendance() {
+      // const data = { 
+      //   memberId: 158,
+      //   currentLatitude: 19.217151,
+      //   currentLongitude: 72.8518034 ,
+      // };
+      // console.log(data);
+
+      let queryParams = `?currentLatitude=19.217151&currentLongitude=72.8518034&memberId=150008`
+      const options = {
+        method: "POST",
+      }
+      let queryURL = "http://164.52.195.248:8062/attendance/member" + queryParams;
+
+      // Mark the attendance for this member
+      fetch(queryURL, options)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        if(data.status == "200") {
+          this.attendanceMarked = true;
+          this.attendanceMsg = data.message;
+          alert(data.message);
+        }
+        console.log(data);
+      })
+      .catch(err => console.log('error: ', err))
+    }
   },
   created() {
     this.event = JSON.parse(localStorage.eventDetails);
